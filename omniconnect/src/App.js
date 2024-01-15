c0e9662e48e3a2ee287cdb3cc3ea9a7a6fd7ccf1
unknown <fahmiemike@gmail.com>
2024-01-14 21:44:20 -0500

    added navbar

--- omniconnect/src/App.js
+++ omniconnect/src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 import {
-  CustomerProfileCollection 
+  CustomerProfileCollection, NavBar
  } from './ui-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar width={"100%"} />
         <CustomerProfileCollection />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
